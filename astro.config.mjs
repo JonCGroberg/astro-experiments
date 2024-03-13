2f2c5af977c639601dd719af45d14a4440c5f7d7
Jonathan Groberg <jongroberg@gmail.com>
2024-03-13 18:00:56 -0400

    added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import netlify from '@astrojs/netlify'
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://joncgroberg.github.io",
-    base: '/astro-experiments'
+  site: "https://joncgroberg.github.io",
+  base: '/astro-experiments',
+  integrations: [react()],
+  output: 'server',
+  adapter: netlify()
 });
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,15 +4,8 @@ import "../styles/button.css";
 function Button(props) {
 	const [count, setCount] = useState(0);
 	return (
-		<button
-			onClick={() => {
-				setCount(count + 1);
-				console.log("test");
-			}}
-			className="btn btn-white"
-		>
-			{" "}
-			<pre>{`${props.text} : ${count}`}</pre>
+		<button onClick={() => setCount(count + 1)} className="btn btn-white">
+			{`${props.text} : ${count}`}
 		</button>
 	);
 }
